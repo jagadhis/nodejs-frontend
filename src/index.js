db4c3a980565dc8ffd50627c02c215e78ffffce6
jagadhis <jwaran78@gmail.com>
2022-11-15 11:34:02 +0530

    Added dashboard and Navbar

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import "bulma/css/bulma.css";
 import reportWebVitals from './reportWebVitals';
+import axios from "axios";
 
+axios.defaults.withCredentials = true;
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
